40c5d833c992437b7486e04054fbc80abcc629cf
Alice-easy <alice-easy@ea.cloudns.ch>
2025-09-12 22:13:19 +0800

    chore(deps): 更新依赖项顺序，启用严格模式并调整TypeScript配置

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  publicDir: 'public',
   resolve: {
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, './src'),
     },
   },
   build: {
